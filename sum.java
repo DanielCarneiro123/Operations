26304a230d4eca6c3c1994afd0219686aceeb0b4
Daniel <danielmc2116@gmail.com>
2022-09-27 10:00:37 +0100

    alteracao

--- sum.java
+++ sum.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
